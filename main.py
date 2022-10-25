d5970acc415c7c498564d76037269e281e822dfd
Guscosta88 <guslfcosta@gmail.com>
2022-10-25 12:48:31 +1100

    controller updated with delete, add, get, etc

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import os
 def create_app():
     app = Flask(__name__)
 
+    @app.errorhandler(404)
+    def not_found(err):
+        return {'error': 'Not Found'}, 404
+
     app.config ['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
